a64682f304be6aaf384b154400bc7bb79a04eebe
juliana thayil <julianathayil@gmail.com>
2020-08-18 14:24:28 +0530

    Client added

---
+++ client/build/precache-manifest.80e2deeebe0bba944ea5314251cb4b59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76f40fcc83b8a0a92312e585325c2cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b5afa1e0264b6ec0b21",
+    "url": "/static/css/2.89d568a6.chunk.css"
+  },
+  {
+    "revision": "9b5afa1e0264b6ec0b21",
+    "url": "/static/js/2.b8634916.chunk.js"
+  },
+  {
+    "revision": "63e46201359a8b084d5e",
+    "url": "/static/js/main.bb09ef13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -23,6 +24,15 @@ const contactsRouter = require('./routes/contacts');
 
 app.use('/birthdays', birthdayRouter);
 app.use('/contacts', contactsRouter);
+
+// Step 3
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static( 'client/build' ));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+  });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
