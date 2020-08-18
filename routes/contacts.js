71730ee485132bddb05301f280849840aedc0d9b
juliana thayil <julianathayil@gmail.com>
2020-08-18 16:49:09 +0530

    Client added

--- routes/contacts.js
+++ routes/contacts.js
@@ -21,4 +21,10 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').delete((req, res) => {
+  Contact.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Contact deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
