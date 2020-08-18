257d3955cbd40ef207ebba40940e406752b004f1
juliana thayil <julianathayil@gmail.com>
2020-08-18 15:03:12 +0530

    Client added

--- server.js
+++ server.js
@@ -25,6 +25,14 @@ const contactsRouter = require('./routes/contacts');
 app.use('/birthdays', birthdayRouter);
 app.use('/contacts', contactsRouter);
 
+// Step 3
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static( 'client/build' ));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+  });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
